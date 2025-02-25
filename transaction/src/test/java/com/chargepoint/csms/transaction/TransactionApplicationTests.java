8268afec5dac40b911083adc09bdb5630ff3b240
payhez <payhez@gmail.com>
2025-02-25 11:48:24 +0100

    add transaction service

---
+++ transaction/src/test/java/com/chargepoint/csms/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chargepoint.csms.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
