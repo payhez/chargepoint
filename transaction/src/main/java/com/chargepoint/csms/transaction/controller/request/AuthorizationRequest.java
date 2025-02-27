d01336451e5b254f10d41a476c05106be7b123f3
payhez <payhez@gmail.com>
2025-02-27 13:01:18 +0100

    add lombok annt

--- transaction/src/main/java/com/chargepoint/csms/transaction/controller/request/AuthorizationRequest.java
+++ transaction/src/main/java/com/chargepoint/csms/transaction/controller/request/AuthorizationRequest.java
@@ -1,10 +1,13 @@
 package com.chargepoint.csms.transaction.controller.request;
 
 import com.chargepoint.csms.transaction.model.DriverIdentifier;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class AuthorizationRequest {
     private UUID stationUuid;
     private DriverIdentifier driverIdentifier;
--- transaction/src/main/java/com/chargepoint/csms/transaction/controller/response/AuthorizationResponse.java
+++ transaction/src/main/java/com/chargepoint/csms/transaction/controller/response/AuthorizationResponse.java
@@ -1,17 +1,11 @@
 package com.chargepoint.csms.transaction.controller.response;
 
 import com.chargepoint.csms.transaction.enums.AuthorizationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class AuthorizationResponse {
     private AuthorizationStatus authorizationStatus;
-
-    public AuthorizationResponse() {}
-
-    public AuthorizationResponse(AuthorizationStatus authorizationStatus) {
-        this.authorizationStatus = authorizationStatus;
-    }
-
-    public AuthorizationStatus getAuthorizationStatus() {
-        return authorizationStatus;
-    }
 }
