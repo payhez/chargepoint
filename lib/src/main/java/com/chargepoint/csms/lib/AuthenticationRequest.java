01aba7005a86ea9733dae376cf0dc00e434cf93f
payhez <payhez@gmail.com>
2025-02-28 13:02:17 +0100

    add no args cons to dtos

--- lib/src/main/java/com/chargepoint/csms/lib/AuthenticationRequest.java
+++ lib/src/main/java/com/chargepoint/csms/lib/AuthenticationRequest.java
@@ -3,11 +3,13 @@ package com.chargepoint.csms.lib;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AuthenticationRequest {
     private UUID requestId;
--- lib/src/main/java/com/chargepoint/csms/lib/AuthenticationResponse.java
+++ lib/src/main/java/com/chargepoint/csms/lib/AuthenticationResponse.java
@@ -3,11 +3,13 @@ package com.chargepoint.csms.lib;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AuthenticationResponse {
     private UUID requestId;
